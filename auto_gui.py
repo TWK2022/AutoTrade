8ad9aa6898ad82de0988e4a969ce0ed162531ae8
twk <1024565378@qq.com>
2025-02-05 01:38:17 +0800

    !

---
+++ auto_gui.py
@@ -0,0 +1,141 @@
+import PIL
+import time
+import yaml
+import logging
+import pyperclip
+import pyautogui
+import numpy as np
+from ocr import ocr_class
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#
+# -------------------------------------------------------------------------------------------------------------------- #
+class auto_gui_class:
+    def __init__(self, yaml_path):
+        with open(yaml_path, 'r', encoding='utf-8') as f:  # 配置文件
+            self.yaml_dict = yaml.load(f, Loader=yaml.SafeLoader)
+        self.h, self.w = pyautogui.size()  # 屏幕大小
+        self.ocr = ocr_class()
+
+    def auto_gui(self):
+        logging.info('--------------------auto_gui--------------------')
+        # 桌面
+        pyautogui.moveTo(1, 1, duration=0)
+        pyautogui.hotkey('win', 'd')
+        time.sleep(0.2)
+        # 打开软件
+        x, y = self._find_click(self.yaml_dict['匹配图片']['同花顺_任务栏'])  # 已存在
+        if x is None:  # 不存在，重新打开
+            x, y = self._find_click(self.yaml_dict['匹配图片']['同花顺_桌面'], click=2)
+            time.sleep(3)
+        if x is None:
+            raise
+        # 放大页面
+        self._find_click(self.yaml_dict['匹配图片']['页面放大'])
+        # 自选
+        self._find_click(self.yaml_dict['匹配图片']['自选'])
+        # 上证指数
+        self._find_click(self.yaml_dict['匹配图片']['上证指数'])
+        # 输入
+        x, y = self.image_location(self.yaml_dict['匹配图片']['搜索'])
+        pyautogui.moveTo(x + 0.05 * self.w, y, duration=0)
+        self.click_delete_input('日历')
+        # 搜索
+        pyautogui.moveTo(x, y, duration=0)
+        pyautogui.click(button='left', clicks=1, interval=0)
+        time.sleep(0.2)
+        # 选项1
+        self._find_click(self.yaml_dict['匹配图片']['选项1'])
+        if x is None:
+            raise
+        # 选项2
+        x, y = self.image_location(self.yaml_dict['匹配图片']['选项2'], retry=5)
+        if x is None:
+            raise
+        image = pyautogui.screenshot('save.jpg', region=(int(x - 0.01 * self.w), int(y + 0.01 * self.h),
+                                                         int(0.03 * self.w), int(0.015 * self.h)))
+        # ocr识别
+        result = self.ocr.ocr(np.array(image))
+        print(result)
+        # 关闭软件
+        # self._find_click(self.yaml_dict['匹配图片']['页面关闭'])
+
+    def _state_judge(self):  # 检查软件状态
+        # 桌面
+        pyautogui.moveTo(1, 1, duration=0)
+        pyautogui.hotkey('win', 'd')
+        time.sleep(0.2)
+        # 检查软件状态
+        try:  # 已经打开
+            self._find_click(self.yaml_dict['匹配图片']['软件_任务栏'], click=2)
+        except:  # 重新启动
+            self._find_click(self.yaml_dict['匹配图片']['软件'])
+        # 位于第1个页面
+        x, y = self.image_location(path1, retry=3)
+        if x is not None:
+            return None
+        # 位于第2个页面
+        x, y = self.image_location(path2, retry=3)
+        if x is not None:
+            return None
+
+    def _find_click(self, image_path, click=1, retry=3):  # 匹配图片并点击
+        x, y = self.image_location(image_path, retry=retry)
+        if x is None:
+            return None, None
+        pyautogui.moveTo(x, y, duration=0)
+        pyautogui.click(button='left', clicks=click, interval=0)
+        time.sleep(0.2)
+        return x, y
+
+    @staticmethod
+    def image_location(image_path, confidence=0.8, retry=3):  # 匹配图片+得到位置坐标
+        def location():
+            try:  # 匹配图片
+                image = PIL.Image.open(image_path)
+                x, y = pyautogui.locateCenterOnScreen(image, confidence=confidence)
+                return x, y
+            except pyautogui.ImageNotFoundException:
+                return None, None
+
+        number = 0
+        sleep = 0.2
+        while number != retry:
+            x, y = location()
+            if x is not None:
+                break
+            time.sleep(sleep)
+            sleep += 0.2
+            number += 1
+        return x, y
+
+    @staticmethod
+    def click_delete_input(text, sleep=0.2):  # 点击+清除+输入文本
+        pyautogui.click(button='left', clicks=1, interval=0)
+        time.sleep(sleep)
+        pyautogui.hotkey('ctrl', 'a', interval=0)
+        pyautogui.hotkey('ctrl', 'x', interval=0)
+        pyperclip.copy(text)
+        pyautogui.hotkey('ctrl', 'v', interval=0)
+
+    @staticmethod
+    def screenshot_measure():  # 截图+测量距离
+        import matplotlib.pyplot as plt
+        from matplotlib.pyplot import MultipleLocator
+        image = pyautogui.screenshot()
+        image = image.resize((1000, 1000))
+        plt.figure(figsize=(20, 20))
+        plt.imshow(image)
+        plt.grid()
+        plt.gca().xaxis.set_major_locator(MultipleLocator(50))
+        plt.gca().yaxis.set_major_locator(MultipleLocator(50))
+        plt.show()
+
+
+if __name__ == '__main__':
+    # logging.basicConfig(filename='log.log', level=logging.INFO, encoding='utf-8',
+    #                     format='%(asctime)s | %(levelname)s | %(message)s')
+    model = auto_gui_class(yaml_path='config/auto_gui.yaml')
+    model.auto_gui()
+    pass
---
+++ ocr.py
@@ -0,0 +1,66 @@
+import cv2
+import argparse
+import onnxruntime
+import numpy as np
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# ocr图片文字检测：https://www.modelscope.cn/models/iic/cv_convnextTiny_ocr-recognition-general_damo
+# 需要文件：model.onnx、vocab.txt
+# 修改:：vocab.txt最前面加两个空行
+# -------------------------------------------------------------------------------------------------------------------- #
+parser = argparse.ArgumentParser(description='|ocr图片文字检测|')
+parser.add_argument('--model_path', default='ocr_model/model.onnx', type=str, help='|ocr模型位置|')
+parser.add_argument('--vocab_path', default='ocr_model/vocab.txt', type=str, help='|vocab词表位置|')
+args, _ = parser.parse_known_args()
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+class ocr_class:
+    def __init__(self, args=args):
+        self.session = onnxruntime.InferenceSession(args.model_path, providers=['CPUExecutionProvider'])  # 模型
+        self.input_name = self.session.get_inputs()[0].name
+        self.output_name = self.session.get_outputs()[0].name
+        with open(args.vocab_path, 'r', encoding='utf-8') as f:  # 词表
+            self.vocab_path = np.array([_[:-1] for _ in f.readlines()])
+
+    def ocr(self, image):
+        input_data = self.image_deal(image)
+        output = self.session.run([self.output_name], {self.input_name: input_data})[0][0]
+        predict = np.argmax(output, axis=1)
+        predict = self.vocab_path[predict]
+        predict = predict[np.where(predict[1:] != predict[:-1])]  # 相邻的不能重复
+        predict = ''.join(predict.tolist())
+        return predict
+
+    @staticmethod
+    def image_deal(image):
+        cur_ratio = image.shape[1] / float(image.shape[0])
+        mask_height = 32
+        mask_width = 804
+        if cur_ratio > float(mask_width) / mask_height:
+            cur_target_height = mask_height
+            cur_target_width = mask_width
+        else:
+            cur_target_height = mask_height
+            cur_target_width = int(mask_height * cur_ratio)
+        image = cv2.resize(image, (cur_target_width, cur_target_height))
+        mask = np.zeros([mask_height, mask_width, 3]).astype(np.uint8)
+        mask[:image.shape[0], :image.shape[1], :] = image
+        image = mask
+        image = np.array(image, dtype=np.float32)
+        image_list = []
+        for i in range(3):
+            left = (300 - 48) * i
+            image_list.append(image[:, left:left + 300, :])
+        input_data = np.concatenate(image_list)
+        input_data = np.resize(input_data / 255, (3, 32, 300, 3))
+        input_data = input_data.transpose(0, 3, 1, 2)
+        return input_data
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+if __name__ == '__main__':
+    model = ocr_class(args)
+    image = cv2.imread('image/img.png')
+    result = model.ocr(image)
+    print(result)
