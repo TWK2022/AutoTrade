c4c327f5f0baef453798c082913745d0a5c82bb9
twk <1024565378@qq.com>
2025-03-04 12:57:53 +0800

    !

--- auto_gui/get_information.py
+++ auto_gui/get_information.py
@@ -12,7 +12,7 @@ else:
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # 功能：自动收集同花顺某个板块中的所有股票f10信息。记录到information.txt。让大模型根据内容来筛选股票
-# 运行条件：进入要收集的行业/概念/板块，点击进入行业的第一支股票，再运行代码
+# 运行条件：进入要收集的行业/概念/板块，点击进入行业的第一支股票，再返回代码并运行
 # -------------------------------------------------------------------------------------------------------------------- #
 parser = argparse.ArgumentParser(description='|收集信息|')
 parser.add_argument('--number', default=100, type=int, help='|收集股票上限|')
@@ -44,6 +44,18 @@ class get_information_class(auto_gui_class):
         x, y, w, h = self.image_location(self.yaml_dict['信息']['主营业务'], assert_=True)
         self.axis['信息']['主营业务'] = (x, y)
         self.screenshot['信息']['主营业务'] = (x + w // 2, y - h // 2, int((0.55 * self.w) // 2), h)
+        # 收入
+        x, y, w, h = self.image_location(self.yaml_dict['信息']['收入'], assert_=True)
+        self.axis['信息']['收入'] = (x, y)
+        self.screenshot['信息']['收入'] = (x + w // 2, y - h // 2, int((0.3 * self.w) // 2), h)
+        # 净利润
+        x, y, w, h = self.image_location(self.yaml_dict['信息']['净利润'], assert_=True)
+        self.axis['信息']['净利润'] = (x, y)
+        self.screenshot['信息']['净利润'] = (x + w // 2, y - h // 2, int((0.3 * self.w) // 2), h)
+        # 毛利率
+        x, y, w, h = self.image_location(self.yaml_dict['信息']['毛利率'], assert_=True)
+        self.axis['信息']['毛利率'] = (x, y)
+        self.screenshot['信息']['毛利率'] = (x + w // 2, y - h // 2, int((0.3 * self.w) // 2), h)
 
     def get_information(self):  # 获取股票的公司信息
         for _ in tqdm.tqdm(range(self.number)):
@@ -57,42 +69,32 @@ class get_information_class(auto_gui_class):
             image1 = np.array(pyautogui.screenshot(region=(x, y, w, h)))
             name_str = self.ocr.ocr(image1)
             search1 = self.regex['名称'].search(name_str)
-            if search1 is not None:
-                name = search1.group(1)
-                code = search1.group(2)
-                if name and self.result.get(name) is not None:  # 循环了一轮
-                    print('| 提前结束 | 循环了一轮 |')
-                    break
-                if code[:2] not in self.screen:
-                    pyautogui.click(button='left', clicks=1, interval=0)  # 下一页
-                    continue
-            else:
+            name = search1.group(1)
+            code = search1.group(2)
+            if name and self.result.get(name) is not None:  # 循环了一轮
+                print('| 提前结束 | 循环了一轮 |')
+                break
+            if code[:2] not in self.screen:  # 非目标股票
                 pyautogui.click(button='left', clicks=1, interval=0)  # 下一页
                 continue
-            # 公司亮点
-            x, y, w, h = self.screenshot['信息']['公司亮点']
-            image2 = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-            data_str1 = self.ocr.ocr(image2)
-            # 主营业务
-            x, y, w, h = self.screenshot['信息']['主营业务']
-            image3 = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-            data_str2 = self.ocr.ocr(image3)
-            # 记录
-            if name is None or data_str1 is None or data_str2 is None:
-                print('! 未检测到数据 !')
-            else:
-                self.result[name] = {}
-                self.result[name]['公司亮点'] = data_str1
-                self.result[name]['主营业务'] = data_str2
+            # 截图
+            image = np.array(pyautogui.screenshot())
+            self.result[name] = {}
+            for key in ['公司亮点', '主营业务', '收入', '净利润', '毛利率']:
+                x, y, w, h = self.screenshot['信息'][key]
+                self.result[name][key] = self.ocr.ocr(image[y:y + h, x:x + w])
+                # self.draw_image(image[y:y + h, x:x + w])
             # 下一页
             pyautogui.click(button='left', clicks=1, interval=0)
-            # self.draw_image(image1)
-            # self.draw_image(image2)
-            # self.draw_image(image3)
         # 记录
         line_all = []
         for name in self.result.keys():
-            output = f'{name} {code} | {self.result[name]["公司亮点"]} | {self.result[name]["主营业务"]}'
+            output = f'{name} | ' \
+                     f'公司亮点：{self.result[name]["公司亮点"]} | ' \
+                     f'主营业务：{self.result[name]["主营业务"]} | ' \
+                     f'收入：{self.result[name]["收入"]} | ' \
+                     f'净利润：{self.result[name]["净利润"]} | ' \
+                     f'毛利率：{self.result[name]["毛利率"]} |'
             line_all.append(output + '\n')
             print(output)
         with open('information.txt', 'w', encoding='utf-8') as f:
