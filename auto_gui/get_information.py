0147d18ed57832c0596bc6c70cbd083d8e554041
twk <1024565378@qq.com>
2025-03-06 23:20:00 +0800

    !

--- auto_gui/get_information.py
+++ auto_gui/get_information.py
@@ -11,11 +11,12 @@ else:
     from .auto_gui import auto_gui_class
 
 # -------------------------------------------------------------------------------------------------------------------- #
-# 功能：自动收集同花顺某个板块中的所有股票f10信息。记录到information.txt。让大模型根据内容来筛选股票
+# 功能：自动收集同花顺某个板块中的所有股票f10信息。记录到information.txt
 # 运行条件：进入要收集的行业/概念/板块，点击进入行业的第一支股票，再返回代码并运行
+# 后续筛选：让大模型根据内容来筛选股票，每50个一组。如：根据以下信息，筛选出与A行业相关性很强、业绩有上涨预期的股票
 # -------------------------------------------------------------------------------------------------------------------- #
 parser = argparse.ArgumentParser(description='|收集信息|')
-parser.add_argument('--number', default=300, type=int, help='|收集股票上限|')
+parser.add_argument('--number', default=500, type=int, help='|收集股票上限|')
 parser.add_argument('--screen', default=['00', '60'], type=list, help='|保留的股票开头|')
 args, _ = parser.parse_known_args()
 
@@ -51,13 +52,13 @@ class get_information_class(auto_gui_class):
             x, y = self.axis['信息']['下一个股']
             pyautogui.moveTo(x, y, duration=0)
             # 等待页面刷新
-            self.image_location(self.yaml_dict['信息']['公司亮点'])
+            time.sleep(0.2)
             # 名称
             x, y, w, h = (int(0.448 * self.w), int(0.083 * self.h), int(0.114 * self.w), int(0.032 * self.h))
             image1 = np.array(pyautogui.screenshot(region=(x, y, w, h)))
             name_str = self.ocr.ocr(image1)
             search1 = self.regex['名称'].search(name_str)
-            name = search1.group(1)
+            name = search1.group(1).strip()
             code = search1.group(2)
             if name and self.result.get(name) is not None:  # 循环了一轮
                 print('| 提前结束 | 循环了一轮 |')
@@ -75,21 +76,21 @@ class get_information_class(auto_gui_class):
             x, y, w, h = self.screenshot['信息']['主营业务']
             self.result[name]['主营业务'] = self.ocr.ocr(image[y:y + h, x:x + w])
             # 收入
-            x, y, w, h = self.image_location(self.yaml_dict['信息']['收入'])
+            x, y, w, h = self.image_location(self.yaml_dict['信息']['收入'], confidence=0.75)
             if x is None:  # 未检测到
                 self.result[name]['收入'] = None
             else:
                 x, y, w, h = (x + w // 2, y - h // 2, int((0.3 * self.w) // 2), h)
                 self.result[name]['收入'] = self.ocr.ocr(image[y:y + h, x:x + w])
             # 净利润
-            x, y, w, h = self.image_location(self.yaml_dict['信息']['净利润'])
+            x, y, w, h = self.image_location(self.yaml_dict['信息']['净利润'], confidence=0.75)
             if x is None:  # 未检测到
                 self.result[name]['净利润'] = None
             else:
                 x, y, w, h = (x + w // 2, y - h // 2, int((0.3 * self.w) // 2), h)
                 self.result[name]['净利润'] = self.ocr.ocr(image[y:y + h, x:x + w])
             # 毛利率
-            x, y, w, h = self.image_location(self.yaml_dict['信息']['毛利率'])
+            x, y, w, h = self.image_location(self.yaml_dict['信息']['毛利率'], confidence=0.75)
             if x is None:  # 未检测到
                 self.result[name]['毛利率'] = None
             else:
